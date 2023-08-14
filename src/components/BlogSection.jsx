1981bfbedcf68f3b896161dfd6dd465df7caca6b
Arzena Akter <arzenaakter@gmail.com>
2023-08-15 05:03:38 +0600

    card update

--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -12,11 +12,11 @@ const BlogSection = () => {
   }, []);
 
   return (
-    <div className="  px-10 lg:px-0 my-20">
+    <div className="  my-20">
       <h1 className=" text-xl lg:text-3xl font-semibold mt-20 mb-5 text-center hover:text-green-700">
         Our Blogs
       </h1>
-      <div className=" w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center gap-8">
+      <div className=" lg:w-[80%] w-[100%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center gap-6  px-10 lg:px-0">
         {Blogs.map((blog) => (
           <SingleBlog key={blog.id} blog={blog}></SingleBlog>
         ))}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,26 +8,26 @@ const NavBar = () => {
     },
     {
       path: "/about",
-      title: "About Us",
+      title: "About",
     },
     {
       path: "/contact",
-      title: "Contact Us",
+      title: "Contact",
     },
   ];
   return (
     <nav className="h-20  bg-green-700 shadow-lg">
-      <div className="container mx-auto flex justify-between items-center text-white pt-5 px-10 lg:px-0">
-        <h1 className="text-2xl font-semibold italic drop-shadow-xl ">
+      <div className="w-[80%] mx-auto flex justify-between items-center text-white pt-5 px-10 lg:px-0">
+        <h1 className="text-xl lg:text-2xl font-semibold italic drop-shadow-xl hidden lg:block ">
           Blog Platform
         </h1>
 
-        <ul className="flex gap-2 items-center justify-center">
+        <ul className="flex gap-4 items-center justify-center ">
           {menuItem.map(({ path, title }) => (
             <li key={path}>
               <NavLink
                 exact={path === "/"}
-                activeClassName="text-black font-bold "
+                activeClassName="text-black  "
                 href={path}>
                 {title}
               </NavLink>
